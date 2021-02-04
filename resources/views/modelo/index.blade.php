33c782e5c91cfca611800a1afff53713866ff6ae
AlexFerLoz96 <alejandrofernandez3b@gmail.com>
2021-02-04 13:27:56 +0100

    Añadiendo estilos a las vistas

--- resources/views/modelo/index.blade.php
+++ resources/views/modelo/index.blade.php
@@ -5,18 +5,28 @@
 @section("header", "modelo")
 
 @section("content")
-    <a href="{{ route('modelo.create') }}">New</a>
-    <table border='1'>
-    @foreach ($modeloList as $modelo)
+    <a href="{{ route('modelo.create') }}" class="btn btn-success">New</a>
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">Modelo</th>
+                <th>Ruta</th>
+                <th>Descripcion</th>
+                <th></th>
+                <th></th>
+            </tr>
+        </thead>
+    @foreach ($modeloList as $modelo) 
         <tr>
+            <td>{{$modelo->id}}</td>
             <td>{{$modelo->ruta}}</td>
             <td>{{$modelo->descripcion}}</td>
-            <td><a href="{{route('modelo.edit', $modelo->id)}}">Edit</a></td>
+            <td><a href="{{route('modelo.edit', $modelo->id)}}" class="btn btn-info">Edit</a></td>
             <td>
                 <form action = "{{route('modelo.destroy', $modelo->id)}}" method="POST" id="delete{{$modelo->id}}">
                     @csrf
                     @method("DELETE")
-                    <input type="button"  onclick="question({{ $modelo->id }})" value="Delete">
+                    <input type="button"  onclick="question({{ $modelo->id }})" value="Delete" class="btn btn-danger">
                 </form>
             </td>
         <br>
