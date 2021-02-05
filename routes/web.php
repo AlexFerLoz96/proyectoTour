db49e71f6d41f4a1c97f5ec40d23d3c2eb53f7ac
vagrant <vagrant@homestead>
2021-02-05 13:30:16 +0000

    Autentificacion en pruebas

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-    
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
 
 Route::get('/user', 'UserController@index')->name('user.index');
 Route::get('/user/create', 'UserController@create')->name('user.create');
