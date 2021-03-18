8c09d8bfbb6b83ad950172009ab610e67e0872df
arturo242 <arturocastill0010@gmail.com>
2021-03-18 13:44:42 +0100

    pequieño error

--- resources/views/comercio/public.blade.php
+++ resources/views/comercio/public.blade.php
@@ -22,7 +22,7 @@
                 <div class="carousel-item active w-100">
                     <div class="w-100 h-100" style="position:absolute;background-image:url('/assets/imgs/comercio/{{$imagen->ruta}}');background-position:center;background-size:100%;filter:blur(5px);background-repeat:no-repeat">
                     </div>
-                    <img src="/assets/imgs/comercio/{{$imagen->ruta}}" style="position:relative;z-index:2;left:50%;transform:translate(-50%);width:70%;" class="d-block h-100" alt="...">
+                    <img src="/assets/imgs/comercio/{{$imagen->ruta}}" style="position:relative;left:50%;transform:translate(-50%);width:70%;" class="d-block h-100" alt="...">
                 </div>
                 @break
                 @endforeach
@@ -31,7 +31,7 @@
                 <div class="carousel-item w-100">
                     <div class="w-100 h-100" style="position:absolute;background-image:url('/assets/imgs/comercio/{{$imagen->ruta}}');background-position:center;background-size:100%;filter:blur(5px);background-repeat:no-repeat">
                     </div>
-                    <img src="/assets/imgs/comercio/{{$imagen->ruta}}" style="position:relative;z-index:2;left:50%;transform:translate(-50%);width:70%;" class="d-block h-100" alt="...">
+                    <img src="/assets/imgs/comercio/{{$imagen->ruta}}" style="position:relative;left:50%;transform:translate(-50%);width:70%;" class="d-block h-100" alt="...">
                 </div>
                 @endforeach
             </div>
