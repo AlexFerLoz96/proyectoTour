de347c48facafcc791e89fdd490984bd9f264262
arturo242 <arturocastill0010@gmail.com>
2021-03-20 20:41:01 +0100

    crud comercios y resenas ok

--- resources/views/resena/create.blade.php
+++ resources/views/resena/create.blade.php
@@ -6,28 +6,28 @@
 
 @section("content")
     @isset($resena)
-        <form action="{{ route('resena.update', ['id' => $resena->id]) }}" method="POST">
+        <form class="bg-light border p-4" action="{{ route('resena.update', ['id' => $resena->id]) }}" method="POST">
         @method("PUT")
     @else
-        <form action="{{ route('resena.store') }}" method="POST">
+        <form class="bg-light border p-4" action="{{ route('resena.store') }}" method="POST">
     @endisset
         @csrf
         <h2>Nueva reseña</h2>
-        Comentario:<textarea rows="10" cols="50" name="comentario" value="{{$resena->comentario ?? '' }}">{{$resena->comentario ?? '' }}</textarea><br>
-        Puntuacion: <input type="number" min="1" max="5" name="puntuacion" value="{{$resena->puntuacion ?? ''}}"><br>
-        Usuario: <select name="user_id" id="">
+        Comentario:<div class="form-group mb-4"><textarea rows=4 class="form-control mt-2 mb-4" name="comentario" value="{{$resena->comentario ?? '' }}">{{$resena->comentario ?? '' }}</textarea></div><br>
+        Puntuacion: <input class="form-control" type="number" min="1" max="5" name="puntuacion" value="{{$resena->puntuacion ?? ''}}"><br>
+        Usuario: <select class="form-control" name="user_id" id="">
             <option >-- Seleccionar usuario --</option>
             @foreach($userList as $user)
                 <option value="{{$user->id}}">{{$user->name}}</option>
             @endforeach
         </select><br>
-        Comercio: <select name="comercio_id" id="">
+        Comercio: <select class="form-control" name="comercio_id" id="">
             <option >-- Seleccionar comercio_id --</option>
             @foreach($comercioList as $comercio)
                 <option value="{{$comercio->id}}">{{$comercio->nombre}}</option>
             @endforeach
         </select><br>
-        <input type="submit">
+        <input class="btn btn-success" type="submit">
         </form>
 
 @endsection
